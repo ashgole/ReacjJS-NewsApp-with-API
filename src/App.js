0e94032b470fd90c1f0ad581ef6d112cbadc960c
Ashish Gole <32222935+ashgole@users.noreply.github.com>
2021-11-15 17:31:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-//https://youtu.be/x9p-4QGh-OI?list=PLu0W_9lII9agx66oZnT6IyhcMIbUMNMdt
-//https://github.com/CodeWithHarry/NewsMonkey-React/commits/master
-
 import NavBar from "./components/NavBar";
 import News from "./components/News";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
